60d4f38449641873654d14b4215acf4461862fcf
坂本大将 <taisho6339@sakamototaishou-no-MacBook-Pro.local>
2018-04-07 23:36:00 +0900

    初期ファイル構築

---
+++ src/main/java/com/gudderi/gudderibatch/GudderiBatchApplication.java
@@ -0,0 +1,12 @@
+package com.gudderi.gudderibatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GudderiBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GudderiBatchApplication.class, args);
+	}
+}
