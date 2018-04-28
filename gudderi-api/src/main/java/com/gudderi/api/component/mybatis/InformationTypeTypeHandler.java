60850daeab321db5f23fc75395effce64d970544
坂本大将 <taisho6339@sakamototaishou-no-MacBook-Pro.local>
2018-04-28 21:37:48 +0900

    MyBatisのEnum変換設定

---
+++ gudderi-api/src/main/java/com/gudderi/api/component/mybatis/InformationTypeTypeHandler.java
@@ -0,0 +1,46 @@
+package com.gudderi.api.component.mybatis;
+
+import com.gudderi.api.enums.Convertible;
+import com.gudderi.api.enums.InformationType;
+
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * TODO: Convertibleを実装したEnumクラスはまとめて動的にTypeHandlerインスタンスを作れるように改良する
+ */
+public class InformationTypeTypeHandler extends BaseTypeHandler<InformationType> {
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, InformationType parameter, JdbcType jdbcType) throws SQLException {
+        ps.setInt(i, parameter.getCode());
+    }
+
+    @Override
+    public InformationType getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        if (cs.wasNull()) {
+            return null;
+        }
+        return Convertible.findByCode(InformationType.class, cs.getInt(columnIndex));
+    }
+
+    @Override
+    public InformationType getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        if (rs.wasNull()) {
+            return null;
+        }
+        return Convertible.findByCode(InformationType.class, rs.getInt(columnName));
+    }
+
+    @Override
+    public InformationType getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        if (rs.wasNull()) {
+            return null;
+        }
+        return Convertible.findByCode(InformationType.class, rs.getInt(columnIndex));
+    }
+}
