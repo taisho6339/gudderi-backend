e95397c77a7c8e0630d6b249891d0808d9b306f2
坂本大将 <taisho6339@sakamototaishou-no-MacBook-Pro.local>
2018-05-01 18:01:58 +0900

    日付フォーマット修正

--- gudderi-core/src/main/java/com.gudderi.core/configuration/GsonConfiguration.java
+++ gudderi-core/src/main/java/com.gudderi.core/configuration/GsonConfiguration.java
@@ -1,14 +1,19 @@
 package com.gudderi.core.configuration;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.text.DateFormat;
 
 @Configuration
 public class GsonConfiguration {
     @Bean
     public Gson gsonInstance() {
-        return new Gson();
+        return new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd HH:mm:ss")
+                .create();
     }
 }
